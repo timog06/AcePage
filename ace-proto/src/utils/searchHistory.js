82ae23cf8b7a7ad3bac83a470f89062c8c54cdb9
Timog <110891995+timog06@users.noreply.github.com>
2025-03-07 13:17:20 +0100

    Delete ace-proto directory

--- ace-proto/src/utils/searchHistory.js
+++
@@ -1,5 +0,0 @@
-export const addToSearchHistory = (id, currentHistory) => {
-  const newHistory = [id, ...currentHistory.filter(item => item !== id)].slice(0, 5);
-  localStorage.setItem('steamIdHistory', JSON.stringify(newHistory));
-  return newHistory;
-};
